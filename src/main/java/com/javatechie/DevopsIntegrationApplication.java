00d4dbc1d29a85c6d069aca85b56a2b75345ccba
anisharnav <96863844+anisharnav@users.noreply.github.com>
2026-07-03 01:47:16 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project by Adarsh Kumar-23:41 !!";
+		return " Re Cpalua Kutta betichor !!";
 	}
 
 	public static void main(String[] args) {
